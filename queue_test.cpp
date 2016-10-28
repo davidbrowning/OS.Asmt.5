19c054ff2a8e0d620c5241fc39100c0ef799de0a
= <=>
2016-10-28 12:57:00 -0600

    took a few minutes to figure out std::unordered_map

--- queue_test.cpp
+++ queue_test.cpp
@@ -6,7 +6,8 @@
 #include<cstdlib>
 #include<chrono>
 #include<utility>
-
+#include<unordered_map>
+//Technically this is a thread and a queue demo
 int getNext(std::mutex& mutex, std::queue<int>& q){
 	std::lock_guard<std::mutex> lock(mutex);
 	int task = q.front();
@@ -14,7 +15,7 @@ int getNext(std::mutex& mutex, std::queue<int>& q){
 	return task;
 }
 
-void threadStart(int id, std::mutex &m, std::queue<int>& q){
+void threadStart(int id, std::mutex &m, std::queue<int>& q, std::unordered_map<int, int>& map){
 	for(int i = 0; i < 20; i++){
 	int task;
 	std::cout << "id: " << id << std::endl;
@@ -22,6 +23,7 @@ void threadStart(int id, std::mutex &m, std::queue<int>& q){
 	task = getNext(m, q);
 	std::cout << "task: " << task << std::endl;
 	std::cout.flush();
+	map.insert({task, 7});
 	//std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 }
@@ -33,13 +35,16 @@ int main(){
 	for(int i = 0; i < 1000; i++){
 		q.push(i);
 	}
+	std::unordered_map<int, int> map;
 	std::mutex mutex;
-	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q));
-	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q));
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(map));
+	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(map));
 	
 	thread1.join();
 	thread2.join();
 	
+	std::cout << map.at(12) << std::endl;
+
 	/*for(int i = 0; i < 1000; i++){
 		std::cout << q.front() << std::endl;
 		fout << q.front() << ',';
