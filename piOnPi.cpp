ea6b73e2a4c91495a3fc5ed4961b905b87c3a468
David Browning <dmbrowning21@gmail.com>
2016-10-29 19:29:37 +0000

    threads now exiting gracefully

--- piOnPi.cpp
+++ piOnPi.cpp
@@ -18,8 +18,9 @@ int getNext(std::mutex& mutex, std::queue<int>& q){
 }
 
 
-void recordAns(std::unordered_map<int, int>& um, int task, int answer){
+void recordAns(std::mutex& mutex, std::unordered_map<int, int>& um, int task, int answer){
 //This is going to work because of the hash table idea
+	std::lock_guard<std::mutex> lock(mutex);
 	um.insert({task, answer});
 	return;
 }
@@ -36,10 +37,10 @@ void threadStart(int id, std::mutex &m, std::queue<int>& q, std::unordered_map<i
 	std::cout << '.';
 	std::cout.flush();
 	ans = computePiDigit(task);
-	recordAns(um, task, ans);
+	recordAns(m, um, task, ans);
 	//std::cout << "ans: " << ans << std::endl;
 	//std::cout.flush();
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	//std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 }
 
@@ -47,36 +48,41 @@ int main(){
 	std::ofstream fout;
 	fout.open("qtest.txt");
 	std::queue<int> q;
-	for(int i = 1; i < 1000; i++){
+	for(int i = 1; i < 1001; i++){ //If I'm starting at 1 I have to end at 1001
 		q.push(i);
 	}
 	std::unordered_map<int, int> um;
 	std::mutex mutex;
 	auto t = std::thread::hardware_concurrency();
 	
+//I'm fully aware that this part is horribly ugly, but I got confused regarding how to make the creation of threads dependent upon an iterator. I tried making a vector of threads but just wound up with a bunch of errors. I figured I start with the 4 case as I feel it is the most likely. 	
+	if(t == 4){
 	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
 	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
 	std::thread thread3(threadStart, 3, std::ref(mutex), std::ref(q), std::ref(um));
 	std::thread thread4(threadStart, 4, std::ref(mutex), std::ref(q), std::ref(um));
-	std::thread thread5(threadStart, 5, std::ref(mutex), std::ref(q), std::ref(um));
-	std::thread thread6(threadStart, 6, std::ref(mutex), std::ref(q), std::ref(um));
-	std::thread thread7(threadStart, 7, std::ref(mutex), std::ref(q), std::ref(um));
-	std::thread thread8(threadStart, 8, std::ref(mutex), std::ref(q), std::ref(um));
-	
-	if(t == 4){
 		thread1.join();
 		thread2.join();
 		thread3.join();
 		thread4.join();
 	}	
 	else if(t == 1){
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
 		thread1.join();
 	}
 	else if(t == 2){
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
 		thread1.join();
 		thread2.join();
 	}
 	else if(t == 6){
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread3(threadStart, 3, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread4(threadStart, 4, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread5(threadStart, 5, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread6(threadStart, 6, std::ref(mutex), std::ref(q), std::ref(um));
 		thread1.join();
 		thread2.join();
 		thread3.join();
@@ -85,6 +91,14 @@ int main(){
 		thread6.join();
 	}
 	else if(t == 8){
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread3(threadStart, 3, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread4(threadStart, 4, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread5(threadStart, 5, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread6(threadStart, 6, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread7(threadStart, 7, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread8(threadStart, 8, std::ref(mutex), std::ref(q), std::ref(um));
 		thread1.join();
 		thread2.join();
 		thread3.join();
