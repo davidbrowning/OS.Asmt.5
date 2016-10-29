3672898833097047beab33e80b07e6abb032c270
David Browning <dmbrowning21@gmail.com>
2016-10-29 04:19:32 +0000

    cores logic -not very hardcore

--- piOnPi.cpp
+++ piOnPi.cpp
@@ -53,15 +53,49 @@ int main(){
 	std::unordered_map<int, int> um;
 	std::mutex mutex;
 	auto t = std::thread::hardware_concurrency();
-	for(int i = 0; i < t; i++){
-		std::thread thr(threadStart, i, std::ref(mutex), std::ref(q), std::ref(um));
-		thr.join();
-	}
-	/*std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
-	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
 	
-	thread1.join();
-	thread2.join();*/
+	std::thread thread1(threadStart, 1, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread2(threadStart, 2, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread3(threadStart, 3, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread4(threadStart, 4, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread5(threadStart, 5, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread6(threadStart, 6, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread7(threadStart, 7, std::ref(mutex), std::ref(q), std::ref(um));
+	std::thread thread8(threadStart, 8, std::ref(mutex), std::ref(q), std::ref(um));
+	
+	if(t == 4){
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		thread4.join();
+	}	
+	else if(t == 1){
+		thread1.join();
+	}
+	else if(t == 2){
+		thread1.join();
+		thread2.join();
+	}
+	else if(t == 6){
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		thread4.join();
+		thread5.join();
+		thread6.join();
+	}
+	else if(t == 8){
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		thread4.join();
+		thread5.join();
+		thread6.join();
+		thread7.join();
+		thread8.join();
+	}
+	//thread1.join();
+	//thread2.join();
 	
 	std::cout << "\n\n3.";
 	//*
